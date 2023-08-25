3764f96fe3f270e52651970c36e9a57331b28760
Jesse Boateng <jesseboateng120@gmail.com>
2023-08-25 10:12:24 +0000

    static component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>Hello World</h1>
-    <p>I am Jesse Adjei Kyereko Boateng, this is my first React application</p>
+    <p>My name is Jesse Adjei Kyereko Boateng. A gradaute with a bachelor's degree in Physics(Geophysics) from KNUST. 
+      I also have a certificate in software development from Ghana-India Kofi Annan Centre of Excellence in ICT (GI-KACE) and 
+      currently a student of Azubi Africa studying frontend development. I am customer relations officer but anticipating to be 
+      a frontend developer in the near future. I have experience in html, CSS, Javascript and React.
+      
+    </p>
     </div>
   );
 }
